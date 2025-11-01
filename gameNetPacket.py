395cdbc43b9d321eb4660d9384804307a2f4b86f
weiennn <huangweien01@gmail.com>
2025-11-01 23:30:53 +0800

    Remove ack flag from gameNetPacket

--- gameNetPacket.py
+++ gameNetPacket.py
@@ -2,19 +2,17 @@
 CHANNEL_TYPE_SIZE = 1
 SEQ_NUM_SIZE = 2
 ACK_NUM_SIZE = 2
-ACK_FLAG_SIZE = 1
 TIME_STAMP_SIZE = 4
 HEADER_SIZE = CHANNEL_TYPE_SIZE + SEQ_NUM_SIZE + \
-    TIME_STAMP_SIZE + ACK_NUM_SIZE + ACK_FLAG_SIZE
+    TIME_STAMP_SIZE + ACK_NUM_SIZE
 
 
 class GameNetPacket:
-    def __init__(self, channel_type=0, seq_num=0, time_stamp=0, ack_num=0, ack_flag=0, payload=b''):
+    def __init__(self, channel_type=0, seq_num=0, time_stamp=0, ack_num=0, payload=b''):
         self.channel_type = channel_type
         self.seq_num = seq_num
         self.time_stamp = time_stamp
         self.ack_num = ack_num
-        self.ack_flag = ack_flag
         self.payload = payload
 
     def to_bytes(self):
@@ -31,14 +29,13 @@ class GameNetPacket:
             raise ValueError("Data is too short to contain a valid header")
         channel_type = data[0]
         seq_num = int.from_bytes(data[1:3], byteorder='big')
-        time_stamp = int.from_bytes(data[3:5], byteorder='big')
-        ack_num = int.from_bytes(data[5:7], byteorder='big')
-        ack_flag = data[7]
+        time_stamp = int.from_bytes(data[3:7], byteorder='big')
+        ack_num = int.from_bytes(data[7:9], byteorder='big')
         payload = data[HEADER_SIZE:]
-        return cls(channel_type, seq_num, time_stamp, ack_num, ack_flag, payload)
+        return cls(channel_type, seq_num, time_stamp, ack_num, payload)
 
     def __repr__(self):
         return (f"GameNetPacket(channel_type={self.channel_type}, "
                 f"seq_num={self.seq_num}, time_stamp={self.time_stamp}, "
-                f"ack_num={self.ack_num}, ack_flag={self.ack_flag}, "
+                f"ack_num={self.ack_num}, "
                 f"payload_length={len(self.payload)})")
